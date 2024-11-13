2af2a6624df313df0423a7ef4232a0537fcbb75d
JanJacek <bobkiewicz.jacek@gmail.com>
2024-11-13 20:10:51 +0100

    Adjust migrations

--- backend_to/prisma/seed.ts
+++ backend_to/prisma/seed.ts
@@ -2,134 +2,85 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-interface Restaurant {
+interface Trader {
   name: string
   email: string
   password: string
   active: boolean
 }
-interface Restaurants {
-  [key: string]: Restaurant
+interface Traders {
+  [key: string]: Trader
 }
 
-const restaurants: Restaurants = {
-  warsaw: {
-    name: 'Warszawa',
-    email: 'warszawa@kawaii.com',
+const traders: Traders = {
+  ala: {
+    name: 'Ala',
+    email: 'ala@ma.com',
     password: 'p123',
     active: true,
   },
-  krakow: {
-    name: 'Kraków',
-    email: 'krakow@kawaii.com',
-    password: 'p123',
-    active: true,
-  },
-  wroclaw: {
-    name: 'Wrocław',
-    email: 'wroclaw@kawaii.com',
-    password: 'p123',
-    active: true,
-  },
-  gdansk: {
-    name: 'Gdańsk',
-    email: 'gdansk@kawaii.com',
-    password: 'p123',
-    active: true,
-  },
-  poznan: {
-    name: 'Poznań',
-    email: 'poznan@kawaii.com',
-    password: 'p123',
-    active: true,
-  },
-  olsztyn: {
-    name: 'Olsztyn',
-    email: 'olsztyn@kawaii.com',
-    password: 'p123',
-    active: true,
-  },
-  lublin: {
-    name: 'Lublin',
-    email: 'lublin@kawaii.com',
+  kot: {
+    name: 'Kot',
+    email: 'kot@ma.com',
     password: 'p123',
     active: true,
   },
 }
 
-const products = [
+const stocks = [
   {
-    name: 'Bāgā',
-    img: 'burger.svg',
-    price: 12,
+    name: 'tesla',
   },
-  {
-    name: 'Roni',
-    img: 'pizza.svg',
-    price: 10,
-  },
-  {
-    name: 'Tanuki',
-    img: 'pita.svg',
-    price: 8,
-  },
-  {
-    name: 'Tenshi',
-    img: 'fries.svg',
-    price: 5,
-  }
 ]
 
-async function addProducts() {
-  const existingProducts = await prisma.products.findMany();
-  if (existingProducts.length === 0) {
-    for (const product of products) {
-      await prisma.products.create({
+async function addStocks() {
+  const existingStocks = await prisma.stocks.findMany();
+  if (existingStocks.length === 0) {
+    for (const stock of stocks) {
+      await prisma.stocks.create({
         data: {
-          name: product.name,
-          img: product.img,
-          price: product.price,
+          name: stock.name,
         },
       });
     }
-    console.log('Products have been created:', products);
+    console.log('Stock has been created:', stocks);
   } else {
-    console.log('Products already exist.');
+    console.log('Stock already exist.');
   }
 }
 
-// Add products
-addProducts().catch((e) => {
+// Add Stocks
+addStocks().catch((e) => {
   console.error(e);
   process.exit(1);
 });
 
 
 
-async function addRestaurants(restaurant: Restaurant) {
-  const existingRestaurant = await prisma.restaurants.findUnique({
-    where: { email: restaurant.email },
+async function addTraders(trader: Trader) {
+  const existingTraders = await prisma.traders.findUnique({
+    where: { email: trader.email },
   });
 
-  if (!existingRestaurant) {
-    await prisma.restaurants.create({
+  if (!existingTraders) {
+    await prisma.traders.create({
       data: {
-        name: restaurant.name,
-        email: restaurant.email,
-        password: restaurant.password,
-        active: restaurant.active,
+        name: trader.name,
+        email: trader.email,
+        password: trader.password,
+        active: trader.active,
       },
     });
 
-    console.log('Admin has been created:', restaurant);
+    console.log('Trader has been created:', trader);
   } else {
-    console.log('Admin already exists.');
+    console.log('Trader already exists.');
   }
 }
 
 async function main() {
-  for (const restaurant of Object.values(restaurants)) {
-    await addRestaurants(restaurant).catch((e) => {
+  for (const trader of Object.values(traders)) {
+    await addTraders(trader).catch((e) => {
       console.error(e);
       process.exit(1);
     });
